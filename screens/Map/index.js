2ec3d9c3e1f39fdf3e4b36a323e07c62265fae02
NoukaDev <amahwied@gmail.com>
2019-06-22 18:25:35 +0200

    feat: get marker from user location

--- screens/Map/index.js
+++ screens/Map/index.js
@@ -7,6 +7,27 @@ import Categories from '../../data/categories'
 import { withNavigation } from 'react-navigation'
 import Theme from '../../constants/Theme'
 
+const geoRange = (coords, radiusInKm) => {
+  const { latitude, longitude } = coords
+
+  const kmInLongitudeDegree = 111.32 * Math.cos(latitude / (180.0 * Math.PI))
+
+  const deltaLat = radiusInKm / 111.1
+  const deltaLong = radiusInKm / kmInLongitudeDegree
+
+  const latitudeGte = latitude - deltaLat
+  const latitudeLt = latitude + deltaLat
+  const longitudeGte = longitude - deltaLong
+  const longitudeLt = longitude + deltaLong
+
+  return {
+    latitudeGte,
+    latitudeLt,
+    longitudeGte,
+    longitudeLt,
+  }
+}
+
 class MapScreen extends Component {
   state = {
     userLocation: undefined,
@@ -24,14 +45,21 @@ class MapScreen extends Component {
   formatedData = data => {
     return data.map(d => {
       const cat = Categories.find(c => c.id + '' === d.categories[0] + '')
-
+      const catFinal = cat || {
+        id: null,
+        color: Theme.color.greenLight,
+        icon: 'fa home',
+        name: '',
+      }
       return {
         ...d,
         category: {
-          id: cat.id,
-          color: cat.color,
-          icon: (cat.icon.split(' ') || ['', '.-.'])[1].replace('fa-', ''),
-          name: cat.name,
+          id: catFinal.id,
+          color: catFinal.color,
+          icon: catFinal.icon
+            ? catFinal.icon.split(' ')[1].replace('fa-', '')
+            : 'home',
+          name: catFinal.name,
         },
       }
     })
@@ -50,8 +78,34 @@ class MapScreen extends Component {
   }
 
   fetch = async () => {
+    const { userLocation } = this.state
+    const reverseGeo = await Location.reverseGeocodeAsync({
+      latitude: userLocation.latitude,
+      longitude: userLocation.longitude,
+    })
+    console.log(reverseGeo)
+    const city = reverseGeo && reverseGeo.length ? reverseGeo[0].city : 'france'
+    const polygon = geoRange(
+      {
+        latitude: userLocation.latitude,
+        longitude: userLocation.longitude,
+      },
+      20
+    )
+    console.log(polygon)
     const rslt = await fetch(
-      'https://transiscope.gogocarto.fr/api/elements/search?text=avignon&limit=20'
+      `https://transiscope.gogocarto.fr/api/elements.json?limit=100&bounds=${
+        polygon.longitudeGte
+      }%2C${polygon.latitudeGte}%2C${polygon.longitudeLt}%2C${
+        polygon.latitudeLt
+      }`
+    )
+    console.log(
+      `https://transiscope.gogocarto.fr/api/elements.json?limit=100&bounds=${
+        polygon.latitudeGte
+      }%2C${polygon.longitudeGte}%2C${polygon.latitudeLt}%2C${
+        polygon.longitudeLt
+      }`
     )
     const jsonObj = await rslt.json()
     const formated = this.formatedData(jsonObj.data)
@@ -76,22 +130,10 @@ class MapScreen extends Component {
               longitudeDelta: 0.0421,
             }}
           >
-            <MapView.Marker
-              coordinate={{
-                latitude: userLocation.latitude,
-                longitude: userLocation.longitude,
-              }}
-              title="Moi"
-              description="Ma position"
-            >
-              <PinIconWrapper color="#5492f744">
-                <UserIcon name="ios-radio-button-on" />
-              </PinIconWrapper>
-            </MapView.Marker>
-
             {data.length &&
               data.map(marker => (
                 <MapView.Marker
+                  key={marker.id}
                   coordinate={{
                     latitude: marker.geo.latitude,
                     longitude: marker.geo.longitude,
@@ -105,6 +147,18 @@ class MapScreen extends Component {
                   </PinIconWrapper>
                 </MapView.Marker>
               ))}
+            <MapView.Marker
+              coordinate={{
+                latitude: userLocation.latitude,
+                longitude: userLocation.longitude,
+              }}
+              title="Moi"
+              description="Ma position"
+            >
+              <PinIconWrapper color="#5492f744">
+                <UserIcon name="ios-radio-button-on" />
+              </PinIconWrapper>
+            </MapView.Marker>
           </MapView>
         ) : (
           <View
