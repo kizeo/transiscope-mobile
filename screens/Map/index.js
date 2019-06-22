c9f8eb9d56ecf904788f78489260d65f48b9fe30
NoukaDev <amahwied@gmail.com>
2019-06-22 15:04:42 +0200

    feat: test Marker on avignon

--- screens/Map/index.js
+++ screens/Map/index.js
@@ -1,44 +1,71 @@
 import React, { Component } from 'react'
-import { MapView } from 'expo'
+import { MapView, Location, Permissions } from 'expo'
 import { Text, ActivityIndicator } from 'react-native'
 import { Container } from '../styled'
+import { PinIcon } from './styled'
 
-class HomeScreen extends Component {
+class MapScreen extends Component {
   state = {
-    data: null,
+    userLocation: undefined,
+    data: [],
   }
-  componentDidMount = () => {
+  componentDidMount = async () => {
+    const userLocation = await this.getLocationAsync()
+    console.log(userLocation)
+    this.setState({
+      userLocation: { ...userLocation.coords },
+    })
     this.fetch()
   }
+
+  getLocationAsync = async () => {
+    const { status } = await Permissions.askAsync(Permissions.LOCATION)
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      })
+    }
+
+    const location = await Location.getCurrentPositionAsync({})
+    return location
+  }
+
   fetch = async () => {
-    console.log('fecth called')
     const rslt = await fetch(
-      'https://transiscope.gogocarto.fr/api/elements.json?limit=10'
+      'https://transiscope.gogocarto.fr/api/elements/search?text=avignon&limit=20'
     )
     const jsonObj = await rslt.json()
-    console.log(jsonObj)
-    console.log(jsonObj.data)
+
     this.setState({
       data: jsonObj.data,
     })
   }
 
   render() {
-    const { data } = this.state
-
+    const { data, userLocation } = this.state
+    console.log(userLocation)
     return (
       <Container>
-        {data ? (
+        {userLocation && data.length ? (
           <MapView
             style={{ flex: 1 }}
             initialRegion={{
-              latitude: -20.89299,
-              longitude: 55.52815,
+              latitude: userLocation.latitude,
+              longitude: userLocation.longitude,
               latitudeDelta: 0.0922,
               longitudeDelta: 0.0421,
             }}
           >
-            {data.length &&
+            <MapView.Marker
+              coordinate={{
+                latitude: userLocation.latitude,
+                longitude: userLocation.longitude,
+              }}
+              title="Moi"
+              description="Ma position"
+            />
+            {false &&
+              data.length &&
               data.map(marker => (
                 <MapView.Marker
                   coordinate={{
@@ -49,6 +76,19 @@ class HomeScreen extends Component {
                   description={marker.abstract}
                 />
               ))}
+            {data.length &&
+              data.map(marker => (
+                <MapView.Marker
+                  coordinate={{
+                    latitude: marker.geo.latitude,
+                    longitude: marker.geo.longitude,
+                  }}
+                  title={marker.name}
+                  description={marker.abstract}
+                >
+                  <PinIcon name="ios-leaf" />
+                </MapView.Marker>
+              ))}
           </MapView>
         ) : (
           <ActivityIndicator size="large" color="#0000ff" />
@@ -58,8 +98,8 @@ class HomeScreen extends Component {
   }
 }
 
-HomeScreen.navigationOptions = {
+MapScreen.navigationOptions = {
   header: null,
 }
 
-export default HomeScreen
+export default MapScreen
