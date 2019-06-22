6d611ba45fb52cd895d2acb1e85429d47c618720
NoukaDev <amahwied@gmail.com>
2019-06-22 11:30:37 +0200

    feat: styled components

---
+++ screens/Home/styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+import Theme from '../../constants/Theme'
+
+export const Container = styled.View`
+  background-color: ${Theme.color.greenLight};
+`
+export const StyledText = styled.Text``
