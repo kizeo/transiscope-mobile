03c2f7b1eac4c40bc0c70e4b312ba775f06984c0
nrcaz <nicholas.rossette@gmail.com>
2019-06-22 18:19:58 +0200

    feature: homeTour slider
    todo left: puces du slider

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-
+import { createAppContainer } from 'react-navigation';
+import createAnimatedSwitchNavigator from 'react-navigation-animated-switch';
+import { Transition } from 'react-native-reanimated';
 import MainTabNavigator from './MainTabNavigator';
+import Slider from '../screens/HomeTour/slider'
 
 export default createAppContainer(
-  createSwitchNavigator({
+  createAnimatedSwitchNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+    HomeTour: Slider,
     Main: MainTabNavigator,
-  })
+  },
+    {
+      // The previous screen will slide to the bottom while the next screen will fade in
+      transition: (
+        <Transition.Together>
+          <Transition.Out
+            type="slide-bottom"
+            durationMs={400}
+            interpolation="easeIn"
+          />
+          <Transition.In type="fade" durationMs={500} />
+        </Transition.Together>
+      ),
+    })
 );
---
+++ screens/HomeTour/firstSlide.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Slide from './slide'
+
+const FirstSlide = () => (
+    <Slide text='Select your city' image={require('../../assets/images/map.png')} />
+)
+
+export default FirstSlide
