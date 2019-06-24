69012d821004403be97bc13f979f163b6d7782db
Igor ESCHALIER <igor.eschalier@gmail.com>
2019-06-24 21:52:44 +0200

    Reload data when region change
    - Remove useless console.log

--- screens/Map/index.js
+++ screens/Map/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-import { MapView, Location, Permissions } from 'expo'
-import { Text, ActivityIndicator, View } from 'react-native'
+import { Location, Permissions } from 'expo'
+import MapView from 'react-native-maps'
+import { ActivityIndicator, View } from 'react-native'
+import { withNavigation } from 'react-navigation'
+
 import { Container } from '../styled'
 import { PinIconFa, UserIcon, PinIconWrapper } from './styled'
 import Categories from '../../data/categories'
-import { withNavigation } from 'react-navigation'
 import Theme from '../../constants/Theme'
 
 const geoRange = (coords, radiusInKm) => {
@@ -33,13 +35,14 @@ class MapScreen extends Component {
     userLocation: undefined,
     data: [],
   }
+
   componentDidMount = async () => {
     const userLocation = await this.getLocationAsync()
-    console.log(userLocation)
+
     this.setState({
-      userLocation: { ...userLocation.coords },
+      userLocation: userLocation.coords,
     })
-    this.fetch()
+    this.fetch(userLocation.coords)
   }
 
   formatedData = data => {
@@ -77,42 +80,33 @@ class MapScreen extends Component {
     return location
   }
 
-  fetch = async () => {
-    const { userLocation } = this.state
-    const reverseGeo = await Location.reverseGeocodeAsync({
-      latitude: userLocation.latitude,
-      longitude: userLocation.longitude,
-    })
-    console.log(reverseGeo)
-    const city = reverseGeo && reverseGeo.length ? reverseGeo[0].city : 'france'
+  fetch = async location => {
     const polygon = geoRange(
       {
-        latitude: userLocation.latitude,
-        longitude: userLocation.longitude,
+        latitude: location.latitude,
+        longitude: location.longitude,
       },
       20
     )
-    console.log(polygon)
+
     const rslt = await fetch(
-      `https://transiscope.gogocarto.fr/api/elements.json?limit=100&bounds=${
+      `https://transiscope.gogocarto.fr/api/elements.json?limit=500&bounds=${
         polygon.longitudeGte
       }%2C${polygon.latitudeGte}%2C${polygon.longitudeLt}%2C${
         polygon.latitudeLt
       }`
     )
-    console.log(
-      `https://transiscope.gogocarto.fr/api/elements.json?limit=100&bounds=${
-        polygon.latitudeGte
-      }%2C${polygon.longitudeGte}%2C${polygon.latitudeLt}%2C${
-        polygon.longitudeLt
-      }`
-    )
+
     const jsonObj = await rslt.json()
     const formated = this.formatedData(jsonObj.data)
-    // console.log(formated)
+
     this.setState({
       data: formated,
     })
+  }
+
+  handleRegionChange = region => {
+    this.fetch(region)
   }
 
   render() {
@@ -120,33 +114,33 @@ class MapScreen extends Component {
     const { navigation } = this.props
     return (
       <Container>
-        {userLocation && data.length ? (
+        {userLocation ? (
           <MapView
             style={{ flex: 1 }}
             initialRegion={{
               latitude: userLocation.latitude,
               longitude: userLocation.longitude,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
+              latitudeDelta: 0.0422,
+              longitudeDelta: 0.0221,
             }}
+            onRegionChangeComplete={this.handleRegionChange}
           >
-            {data.length &&
-              data.map(marker => (
-                <MapView.Marker
-                  key={marker.id}
-                  coordinate={{
-                    latitude: marker.geo.latitude,
-                    longitude: marker.geo.longitude,
-                  }}
-                  title={marker.name}
-                  description={marker.abstract}
-                  onCalloutPress={() => navigation.navigate('MapPinDetail')}
-                >
-                  <PinIconWrapper color={marker.category.color}>
-                    <PinIconFa name={marker.category.icon} />
-                  </PinIconWrapper>
-                </MapView.Marker>
-              ))}
+            {data.map(marker => (
+              <MapView.Marker
+                key={marker.id}
+                coordinate={{
+                  latitude: marker.geo.latitude,
+                  longitude: marker.geo.longitude,
+                }}
+                title={marker.name}
+                description={marker.abstract}
+                onCalloutPress={() => navigation.navigate('MapPinDetail')}
+              >
+                <PinIconWrapper color={marker.category.color}>
+                  <PinIconFa name={marker.category.icon} />
+                </PinIconWrapper>
+              </MapView.Marker>
+            ))}
             <MapView.Marker
               coordinate={{
                 latitude: userLocation.latitude,
