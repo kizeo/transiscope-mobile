323d06f0fe0af0268511280a90440f1bc02308b6
Nicholas <rossette@kizeo.com>
2019-10-28 14:40:37 +0100

    chore: puces to slider + responsive for tablet

--- screens/HomeTour/Carousel/slide.js
+++ screens/HomeTour/Carousel/slide.js
@@ -1,26 +1,26 @@
 import React from 'react'
-import { Image, Text, View, StyleSheet, Dimensions } from 'react-native'
+import {
+  Image, Text, View, StyleSheet, Dimensions
+} from 'react-native'
 
 const Slide = ({ text, image }) => {
-    const { width } = Dimensions.get('window');
-    return (
-        <View style={{ width, alignItems: 'center' }}>
-            <Image style={styles.image} source={image} resizeMode='contain' />
-            <Text style={styles.text}>
-                {text}
-            </Text>
-        </View>
-    )
+  const { width } = Dimensions.get('screen')
+  return (
+    <View style={{ width, alignItems: 'center' }}>
+      <Image style={styles.image} source={image} resizeMode="contain" />
+      <Text style={styles.text}>{text}</Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    image: {
-        width: 300,
-        height: 300
-    },
-    text: {
-        marginBottom: 20
-    }
+  image: {
+    width: 300,
+    height: 300,
+  },
+  text: {
+    marginBottom: 20,
+  },
 })
 
 export default Slide
--- screens/HomeTour/index.js
+++ screens/HomeTour/index.js
@@ -1,22 +1,27 @@
 import React from 'react'
-import { Container, Logo, Button } from './styled'
+import {
+  Container, Logo, Button, BtnTitle
+} from './styled'
 import Carousel from './Carousel'
+import map from '../../assets/images/map.png'
+import bulb from '../../assets/images/bulb.png'
+import rocket from '../../assets/images/rocket.png'
+import logoByKizeo from '../../assets/images/logoByKizeo.png'
 
 const slides = [
-    { text: 'Select your city', file: require('../../assets/images/map.png') },
-    { text: 'Choose a domain', file: require('../../assets/images/bulb.png') },
-    { text: 'Start your eco-journey!', file: require('../../assets/images/rocket.png') }
+  { text: 'Select your city', file: map },
+  { text: 'Choose a domain', file: bulb },
+  { text: 'Start your eco-journey!', file: rocket },
 ]
 
-const HomeTour = ({ navigation }) => {
-
-    return (
-        <Container>
-            <Logo source={require('../../assets/images/logoByKizeo.png')} resizeMode='contain' />
-            <Carousel slides={slides} />
-            <Button title="Let's get started!" onPress={() => navigation.navigate('Main')} />
-        </Container>
-    )
-}
+const HomeTour = ({ navigation }) => (
+  <Container>
+    <Logo source={logoByKizeo} resizeMode="contain" />
+    <Carousel slides={slides} />
+    <Button onPress={() => navigation.navigate('Main')} color="mediumseagreen">
+      <BtnTitle>Let's get started!</BtnTitle>
+    </Button>
+  </Container>
+)
 
 export default HomeTour
